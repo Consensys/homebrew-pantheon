cac17992c7a605444b50df226cd364ee5091e01c
Adrian Sutton <adrian.sutton@consensys.net>
2019-01-16 09:26:10 +1000

    Update to Pantheon 0.8.5.

--- pantheon.rb
+++ pantheon.rb
@@ -1,9 +1,9 @@
 class Pantheon < Formula
   desc "pantheon ethereum client"
   homepage "https://github.com/pegasyseng/pantheon"
-  url "https://consensys.bintray.com/pegasys-repo/pantheon-0.8.4.zip"
+  url "https://consensys.bintray.com/pegasys-repo/pantheon-0.8.5.zip"
   # update with: shasum -a 256
-  sha256 "163cd6f160fdb3cf010aadc190753db96c75f5826897719758ef7c5aa23c6e15"
+  sha256 "d84f3189de8c8328997273ecec82b4f32826ac4afbb934f41083a2d6fb240d8f"
 
   depends_on :java => "1.8+"
 
