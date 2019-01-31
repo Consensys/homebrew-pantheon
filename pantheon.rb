811b139e32e889e3ae63a6eb4255560a356b3208
shemnon <danno.ferrin@gmail.com>
2019-01-31 10:44:46 -0700

    Update to 0.9.1 release

--- pantheon.rb
+++ pantheon.rb
@@ -1,9 +1,9 @@
 class Pantheon < Formula
   desc "pantheon ethereum client"
   homepage "https://github.com/pegasyseng/pantheon"
-  url "https://consensys.bintray.com/pegasys-repo/pantheon-0.9.0.zip"
+  url "https://consensys.bintray.com/pegasys-repo/pantheon-0.9.1.zip"
   # update with: shasum -a 256
-  sha256 "59a5ee0fe2a16e33d777d7b4b7b81babeb06e483ed2d942163e9c96fa1b1c47f"
+  sha256 "21adc39f04b3fb16d66d2e254a9915c2201234af60a79925ad4f980f2de3d052"
 
   depends_on :java => "1.8+"
 
