3072a4fb95d58ee122e598246868d16848a32308
Adrian Sutton <adrian.sutton@consensys.net>
2019-04-30 15:50:27 +1000

    Add script to automate update process.

--- pantheon.rb
+++ pantheon.rb
@@ -2,7 +2,7 @@ class Pantheon < Formula
   desc "pantheon ethereum client"
   homepage "https://github.com/pegasyseng/pantheon"
   url "https://consensys.bintray.com/pegasys-repo/pantheon-1.1.0.zip"
-  # update with: shasum -a 256
+  # update with: ./updatePantheon.sh <new-version>
   sha256 "38162bacde1ee5a95bc4a7f688a6c037a91d0a808d071cc51ddc9c1cea71a157"
 
   depends_on :java => "1.8+"
