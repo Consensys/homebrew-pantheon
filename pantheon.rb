8277117cf73ae1cef889b3fa51a9d7c39407f13e
Edward Evans <edward.evans@consensys.net>
2019-09-12 09:47:06 +1000

    Update 1.2.3
    Signed-off-by: Edward Evans <edward.evans@consensys.net>

--- pantheon.rb
+++ pantheon.rb
@@ -1,9 +1,9 @@
 class Pantheon < Formula
   desc "pantheon ethereum client"
   homepage "https://github.com/pegasyseng/pantheon"
-  url "https://consensys.bintray.com/pegasys-repo/pantheon-1.2.2.zip"
+  url "https://consensys.bintray.com/pegasys-repo/pantheon-1.2.3.zip"
   # update with: ./updatePantheon.sh <new-version>
-  sha256 "4711ba54e961e14fe98baadd864355b79e57cdda4b851ad1dd65ad07566ea4da"
+  sha256 "1593196f14f78c1fa569b4fc343d4c88d5f805654865deb4c3865cbc4c336f09"
 
   depends_on :java => "11+"
 
